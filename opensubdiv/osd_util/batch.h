23ba172397e789d529f5306cc70fe3acca471f02
Takahito Tejima <takahito@pixar.com>
2013-05-09 19:18:05 -0700

    First draft of OsdUtil batching API

---
+++ opensubdiv/osd_util/batch.h
@@ -0,0 +1,338 @@
+//
+//     Copyright (C) Pixar. All rights reserved.
+//
+//     This license governs use of the accompanying software. If you
+//     use the software, you accept this license. If you do not accept
+//     the license, do not use the software.
+//
+//     1. Definitions
+//     The terms "reproduce," "reproduction," "derivative works," and
+//     "distribution" have the same meaning here as under U.S.
+//     copyright law.  A "contribution" is the original software, or
+//     any additions or changes to the software.
+//     A "contributor" is any person or entity that distributes its
+//     contribution under this license.
+//     "Licensed patents" are a contributor's patent claims that read
+//     directly on its contribution.
+//
+//     2. Grant of Rights
+//     (A) Copyright Grant- Subject to the terms of this license,
+//     including the license conditions and limitations in section 3,
+//     each contributor grants you a non-exclusive, worldwide,
+//     royalty-free copyright license to reproduce its contribution,
+//     prepare derivative works of its contribution, and distribute
+//     its contribution or any derivative works that you create.
+//     (B) Patent Grant- Subject to the terms of this license,
+//     including the license conditions and limitations in section 3,
+//     each contributor grants you a non-exclusive, worldwide,
+//     royalty-free license under its licensed patents to make, have
+//     made, use, sell, offer for sale, import, and/or otherwise
+//     dispose of its contribution in the software or derivative works
+//     of the contribution in the software.
+//
+//     3. Conditions and Limitations
+//     (A) No Trademark License- This license does not grant you
+//     rights to use any contributor's name, logo, or trademarks.
+//     (B) If you bring a patent claim against any contributor over
+//     patents that you claim are infringed by the software, your
+//     patent license from such contributor to the software ends
+//     automatically.
+//     (C) If you distribute any portion of the software, you must
+//     retain all copyright, patent, trademark, and attribution
+//     notices that are present in the software.
+//     (D) If you distribute any portion of the software in source
+//     code form, you may do so only under this license by including a
+//     complete copy of this license with your distribution. If you
+//     distribute any portion of the software in compiled or object
+//     code form, you may only do so under a license that complies
+//     with this license.
+//     (E) The software is licensed "as-is." You bear the risk of
+//     using it. The contributors give no express warranties,
+//     guarantees or conditions. You may have additional consumer
+//     rights under your local laws which this license cannot change.
+//     To the extent permitted under your local laws, the contributors
+//     exclude the implied warranties of merchantability, fitness for
+//     a particular purpose and non-infringement.
+//
+#ifndef OSD_UTIL_MESH_BATCH_H
+#define OSD_UTIL_MESH_BATCH_H
+
+#include "../version.h"
+#include "../far/multiMeshFactory.h"
+#include "../far/patchTables.h"
+#include "../osd/vertexDescriptor.h"
+#include "../osd_util/meshHandle.h"
+
+#include <vector>
+
+namespace OpenSubdiv {
+namespace OPENSUBDIV_VERSION {
+
+template <class U> class FarMesh;
+class OsdVertex;
+
+// ----------------------------------------------------------------------------
+// OsdUtilMeshBatchBase
+//
+//  XXX: abstract...?
+//  base batching class which holds farMultiMesh and range information of mesh handles.
+// 
+template <typename DRAW_CONTEXT>
+class OsdUtilMeshBatchBase {
+public:
+    typedef DRAW_CONTEXT DrawContext;
+
+    virtual ~OsdUtilMeshBatchBase();
+
+    virtual typename DrawContext::VertexBufferBinding BindVertexBuffer() = 0;
+    virtual DrawContext * GetDrawContext() const = 0;
+
+    int GetVertexOffset(int meshIndex) const { return _vertexOffsets[meshIndex]; }
+    int GetPtexFaceOffset(int meshIndex) const { return _ptexFaceOffsets[meshIndex]; }
+    int GetQuadOffset(int meshIndex) const { return _quadOffsets[meshIndex]; }
+    int GetQuadCount(int meshIndex) const { return _quadCounts[meshIndex]; }
+
+    virtual void InitializeVertexBuffers(int numVertexElements, int numVaryingElements) = 0;
+
+    // similar methods to OsdGLMesh
+    virtual void UpdateCoarseVertices(const OsdUtilMeshHandle &handle, const float *ptrs, int numVertices) = 0;
+    virtual void FinalizeUpdate() = 0;
+ 
+    OsdDrawContext::PatchArrayVector const & GetPatchArrays(const OsdUtilMeshHandle &meshHandle) const;
+
+    const typename OsdUtilMeshHandle::Collection & GetMeshHandles() const { return _meshHandles; }
+
+    int GetBatchIndex() const { return _batchIndex; }
+
+    int GetNumPtexFaces() const { return _numPtexFaces; }
+
+protected:
+    OsdUtilMeshBatchBase(const std::vector<FarMesh<OsdVertex> const * > &meshVector, int batchIndex);
+
+    void setMeshDirty(int meshIndex);
+    void resetMeshDirty();
+    void updatePatchArrays(int numVertexElements);
+
+    FarKernelBatchVector getKernelBatches();
+
+    FarMesh <OsdVertex> * _GetFarMesh() const { return _farMultiMesh; }
+
+private:
+    typename OsdUtilMeshHandle::Collection _meshHandles;
+    FarMesh <OsdVertex> *_farMultiMesh;
+    std::vector<OsdDrawContext::PatchArrayVector> _multiPatchArrays;
+    FarKernelBatchVector _allKernelBatches;
+
+    std::vector<int>           _vertexOffsets;
+    std::vector<int>           _ptexFaceOffsets;
+    std::vector<bool>          _dirtyFlags;
+
+    std::vector<int>           _quadOffsets;  // for uniform subdivision
+    std::vector<int>           _quadCounts;   // for uniform subdivision
+
+    int _batchIndex;
+    int _numPtexFaces;
+};
+
+// ----------------------------------------------------------------------------
+// OsdUtilMeshBatch
+//
+//  derived batching class which contains vertexbuffer, computecontext, drawcontext
+// 
+template <typename VERTEX_BUFFER, typename DRAW_CONTEXT, typename COMPUTE_CONTROLLER>
+class OsdUtilMeshBatch : public OsdUtilMeshBatchBase<DRAW_CONTEXT> {
+public:
+    typedef VERTEX_BUFFER VertexBuffer;
+    typedef typename COMPUTE_CONTROLLER::ComputeContext ComputeContext;
+    typedef DRAW_CONTEXT DrawContext;
+    typedef COMPUTE_CONTROLLER ComputeController;
+    typedef OsdUtilMeshBatchBase<DRAW_CONTEXT> Base;
+    
+    // XXX: not happy with retaining compute controller..
+    OsdUtilMeshBatch(COMPUTE_CONTROLLER *computeController,
+                     const std::vector<FarMesh<OsdVertex> const * > &meshVector, int batchIndex);
+
+    virtual ~OsdUtilMeshBatch();
+
+    virtual void InitializeVertexBuffers(int numVertexElements, int numVaryingElements);
+
+    virtual typename DrawContext::VertexBufferBinding BindVertexBuffer() { return _vertexBuffer->BindVBO(); }
+    virtual DrawContext * GetDrawContext() const { return _drawContext; }
+
+    // update API
+    virtual void UpdateCoarseVertices(const OsdUtilMeshHandle &handle,
+                                      const float *ptrs, int numVertices) {
+
+        assert(handle.batchIndex == Base::GetBatchIndex());
+
+        Base::setMeshDirty(handle.meshIndex);
+
+        _vertexBuffer->UpdateData(ptrs, Base::GetVertexOffset(handle.meshIndex), numVertices);
+    }
+
+    // utility function
+    virtual void FinalizeUpdate() {
+        // create kernel batch for dirty handles
+        
+        FarKernelBatchVector batches = Base::getKernelBatches();
+        Base::resetMeshDirty();
+
+        _computeController->Refine(_computeContext, batches, _vertexBuffer);
+    }
+
+    VertexBuffer *GetVertexBuffer() const { return _vertexBuffer; }
+    VertexBuffer *GetVaryingBuffer() const { return _varyingBuffer; }
+
+private:
+    ComputeController *_computeController;
+    ComputeContext *_computeContext;
+
+    VertexBuffer *_vertexBuffer, *_varyingBuffer;
+    DrawContext *_drawContext;
+};
+
+//typedef std::vector<OsdUtilMeshBatch*> OsdUtilMeshBatchPtrVector;
+
+// -----------------------------------------------------------------------------
+template <typename DRAW_CONTEXT>
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::
+    OsdUtilMeshBatchBase(const std::vector<FarMesh<OsdVertex> const * > &meshVector, int batchIndex) :
+        _farMultiMesh(NULL), _batchIndex(batchIndex) {
+
+    // create multimesh
+    FarMultiMeshFactory<OsdVertex> multiMeshFactory;
+    _farMultiMesh = multiMeshFactory.Create(meshVector);
+
+    // copy batches
+    _allKernelBatches = _farMultiMesh->GetKernelBatches();
+
+    FarPatchTables const * patchTables = _farMultiMesh->GetPatchTables();
+    
+    // prepare patch arrays
+    std::vector<FarPatchTables::PatchArrayVector> const & multiFarPatchArray = multiMeshFactory.GetMultiPatchArrays();
+
+    // create osd patch array per mesh (note: numVertexElements will be updated later on InitializeVertexBuffers)
+    _multiPatchArrays.resize(multiFarPatchArray.size());
+    for (int i = 0; i < (int)multiFarPatchArray.size(); ++i) {
+        OsdDrawContext::ConvertPatchArrays(multiFarPatchArray[i], _multiPatchArrays[i], patchTables->GetMaxValence(), 0);
+    }
+
+    bool adaptive = (patchTables != 0);
+
+    _numPtexFaces = _farMultiMesh->GetNumPtexFaces();
+
+    // create mesh handles and prepare patch offsets
+    _meshHandles.reserve(meshVector.size());
+    int vertexOffset = 0, ptexFaceOffset = 0, quadOffset = 0;
+    for (size_t i = 0; i < meshVector.size(); ++i) {
+        int numVertices = meshVector[i]->GetNumVertices();
+        int numPtexFaces = meshVector[i]->GetNumPtexFaces();
+
+        int maxLevel = meshVector[i]->GetSubdivisionTables()->GetMaxLevel();
+        int numQuads = adaptive ? 0 : meshVector[i]->GetFaceVertices(maxLevel-1).size()/4;
+
+        _meshHandles.push_back(OsdUtilMeshHandle(batchIndex, (int)i));
+
+        _vertexOffsets.push_back(vertexOffset);
+        _ptexFaceOffsets.push_back(ptexFaceOffset);
+        _quadOffsets.push_back(quadOffset);
+        _quadCounts.push_back(numQuads);
+
+        vertexOffset += numVertices;
+        ptexFaceOffset += numPtexFaces;
+
+        quadOffset += numQuads;
+    }
+    _dirtyFlags.resize(meshVector.size());
+    resetMeshDirty();
+}
+template <typename DRAW_CONTEXT>
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::~OsdUtilMeshBatchBase() {
+    delete _farMultiMesh;
+}
+
+template <typename DRAW_CONTEXT> void
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::setMeshDirty(int meshIndex) {
+
+    assert(meshIndex < (int)_dirtyFlags.size());
+    _dirtyFlags[meshIndex] = true;
+}
+
+template <typename DRAW_CONTEXT> void
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::resetMeshDirty() {
+    std::fill(_dirtyFlags.begin(), _dirtyFlags.end(), false);
+}
+
+template <typename DRAW_CONTEXT> void
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::updatePatchArrays(int numVertexElements) {
+    for (int i = 0; i < (int)_multiPatchArrays.size(); ++i) {
+        for (int j = 0; j < (int)_multiPatchArrays[i].size(); ++j) {
+            OsdDrawContext::PatchDescriptor desc = _multiPatchArrays[i][j].GetDescriptor();
+            desc.SetNumElements(numVertexElements);
+            _multiPatchArrays[i][j].SetDescriptor(desc);
+        }
+    }
+}
+
+template <typename DRAW_CONTEXT> FarKernelBatchVector
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::getKernelBatches() {
+    FarKernelBatchVector result;
+    for (FarKernelBatchVector::const_iterator it = _allKernelBatches.begin();
+         it != _allKernelBatches.end(); ++it) {
+        if (_dirtyFlags[it->GetMeshIndex()]) {
+            result.push_back(*it);
+        }
+    }
+    return result;
+}
+
+template <typename DRAW_CONTEXT> OsdDrawContext::PatchArrayVector const &
+OsdUtilMeshBatchBase<DRAW_CONTEXT>::GetPatchArrays(const OsdUtilMeshHandle &meshHandle) const {
+    return _multiPatchArrays[meshHandle.meshIndex];
+}
+
+// -----------------------------------------------------------------------------
+template <typename VERTEX_BUFFER, typename DRAW_CONTEXT, typename COMPUTE_CONTROLLER>
+OsdUtilMeshBatch<VERTEX_BUFFER, DRAW_CONTEXT, COMPUTE_CONTROLLER>::
+    OsdUtilMeshBatch(COMPUTE_CONTROLLER *computeController,
+                     const std::vector<FarMesh<OsdVertex> const * > &meshVector, int batchIndex) :
+         OsdUtilMeshBatchBase<DRAW_CONTEXT>(meshVector, batchIndex),
+             _computeController(computeController),
+             _computeContext(NULL), _vertexBuffer(NULL), _varyingBuffer(NULL), _drawContext(NULL) {
+        
+    // create compute contexts
+    _computeContext = ComputeContext::Create(Base::_GetFarMesh());
+}
+
+template <typename VERTEX_BUFFER, typename DRAW_CONTEXT, typename COMPUTE_CONTROLLER>
+OsdUtilMeshBatch<VERTEX_BUFFER, DRAW_CONTEXT, COMPUTE_CONTROLLER>::~OsdUtilMeshBatch() {
+    delete _computeContext;
+    delete _drawContext;
+
+    delete _vertexBuffer;
+    delete _varyingBuffer;
+}
+
+template <typename VERTEX_BUFFER, typename DRAW_CONTEXT, typename COMPUTE_CONTROLLER> void
+OsdUtilMeshBatch<VERTEX_BUFFER, DRAW_CONTEXT, COMPUTE_CONTROLLER>::InitializeVertexBuffers(int numVertexElements, int numVaryingElements) {
+
+    delete _vertexBuffer;
+    delete _varyingBuffer;
+    delete _drawContext;
+
+    int numVerts = Base::_GetFarMesh()->GetNumVertices();
+    _vertexBuffer = numVertexElements ? VertexBuffer::Create(numVertexElements, numVerts) : NULL;
+    _varyingBuffer = numVaryingElements ? VertexBuffer::Create(numVaryingElements, numVerts) : NULL;
+    _drawContext = DrawContext::Create(Base::_GetFarMesh(), _vertexBuffer, /*fvar=*/false);
+    assert(_drawContext);
+
+    // update patch arrays
+    Base::updatePatchArrays(numVertexElements);
+}
+
+}  // end namespace OPENSUBDIV_VERSION
+using namespace OPENSUBDIV_VERSION;
+
+}  // end namespace OpenSubdiv
+
+#endif  /* OSD_UTIL_MESH_BATCH_H */
---
+++ opensubdiv/osd_util/drawController.h
@@ -0,0 +1,234 @@
+//
+//     Copyright (C) Pixar. All rights reserved.
+//
+//     This license governs use of the accompanying software. If you
+//     use the software, you accept this license. If you do not accept
+//     the license, do not use the software.
+//
+//     1. Definitions
+//     The terms "reproduce," "reproduction," "derivative works," and
+//     "distribution" have the same meaning here as under U.S.
+//     copyright law.  A "contribution" is the original software, or
+//     any additions or changes to the software.
+//     A "contributor" is any person or entity that distributes its
+//     contribution under this license.
+//     "Licensed patents" are a contributor's patent claims that read
+//     directly on its contribution.
+//
+//     2. Grant of Rights
+//     (A) Copyright Grant- Subject to the terms of this license,
+//     including the license conditions and limitations in section 3,
+//     each contributor grants you a non-exclusive, worldwide,
+//     royalty-free copyright license to reproduce its contribution,
+//     prepare derivative works of its contribution, and distribute
+//     its contribution or any derivative works that you create.
+//     (B) Patent Grant- Subject to the terms of this license,
+//     including the license conditions and limitations in section 3,
+//     each contributor grants you a non-exclusive, worldwide,
+//     royalty-free license under its licensed patents to make, have
+//     made, use, sell, offer for sale, import, and/or otherwise
+//     dispose of its contribution in the software or derivative works
+//     of the contribution in the software.
+//
+//     3. Conditions and Limitations
+//     (A) No Trademark License- This license does not grant you
+//     rights to use any contributor's name, logo, or trademarks.
+//     (B) If you bring a patent claim against any contributor over
+//     patents that you claim are infringed by the software, your
+//     patent license from such contributor to the software ends
+//     automatically.
+//     (C) If you distribute any portion of the software, you must
+//     retain all copyright, patent, trademark, and attribution
+//     notices that are present in the software.
+//     (D) If you distribute any portion of the software in source
+//     code form, you may do so only under this license by including a
+//     complete copy of this license with your distribution. If you
+//     distribute any portion of the software in compiled or object
+//     code form, you may only do so under a license that complies
+//     with this license.
+//     (E) The software is licensed "as-is." You bear the risk of
+//     using it. The contributors give no express warranties,
+//     guarantees or conditions. You may have additional consumer
+//     rights under your local laws which this license cannot change.
+//     To the extent permitted under your local laws, the contributors
+//     exclude the implied warranties of merchantability, fitness for
+//     a particular purpose and non-infringement.
+//
+#ifndef OSD_UTIL_DRAW_CONTROLLER_H
+#define OSD_UTIL_DRAW_CONTROLLER_H
+
+#include "../version.h"
+
+#include "../osd/patch.h"
+
+namespace OpenSubdiv {
+namespace OPENSUBDIV_VERSION {
+
+/*
+  concept DrawDelegate
+  {
+    void BindBatch(OsdUtilMeshBatchBase<DRAW_CONTEXT> *batch);
+    void BindEffect(EFFECT *effect);
+    void DrawElements(EFFECT *effect, OsdDrawContext::PatchArray const &patchArray);
+  }
+*/
+
+namespace OsdUtil {
+
+    // ------------------------------------------------------------------------
+    // DrawCollection
+    // ------------------------------------------------------------------------
+    template <typename DRAW_ITEM_COLLECTION, typename DRAW_DELEGATE>
+    void DrawCollection(DRAW_ITEM_COLLECTION const &items, DRAW_DELEGATE *delegate) {
+
+        typedef typename DRAW_ITEM_COLLECTION::value_type DrawItem;
+
+        // XXX: Are these caches needed in this class? user delegate can do that?
+        bool first = true;
+        typename DrawItem::BatchBase *currentBatch = NULL;
+        typename DrawItem::EffectHandle currentEffect; // XXX: initial value for pointer?
+    
+        // iterate over DrawItemCollection
+        for (typename DRAW_ITEM_COLLECTION::const_iterator it = items.begin(); it != items.end(); ++it) {
+            typename DrawItem::BatchBase *batch = it->GetBatch();
+            typename DrawItem::EffectHandle effect = it->GetEffect();
+            if (first || currentBatch != batch) {
+                if (not first) delegate->UnbindBatch(currentBatch);
+                delegate->BindBatch(batch);
+                currentBatch = batch;
+            }
+            if (first || currentEffect != effect) {
+                if (not first) delegate->UnbindEffect(currentEffect);
+                delegate->BindEffect(effect);
+                currentEffect = effect;
+            }
+
+            // iterate over sub items within a draw item
+            OsdDrawContext::PatchArrayVector const &patchArrays = it->GetPatchArrays();
+            for (OsdDrawContext::PatchArrayVector::const_iterator pit = patchArrays.begin(); pit != patchArrays.end(); ++pit) {
+                delegate->DrawElements(currentEffect, *pit);
+            }
+
+            first = false;
+        }
+        if (not first) delegate->UnbindEffect(currentEffect);
+        if (not first) delegate->UnbindBatch(currentBatch);
+    }
+
+    // ------------------------------------------------------------------------
+    struct PatchArrayCombiner {
+        typedef std::vector<std::pair<OsdDrawContext::PatchDescriptor, OsdDrawContext::PatchArrayVector> > Dictionary;
+
+        struct PatchArrayComparator {
+            bool operator() (OsdDrawContext::PatchArray const &a, OsdDrawContext::PatchArray const &b) {
+                return a.GetDescriptor() < b.GetDescriptor() or ((a.GetDescriptor() == b.GetDescriptor()) and
+                                                                 (a.GetVertIndex() < b.GetVertIndex()));
+            }
+        };
+
+        // XXX: reconsider this function
+        template <typename DRAW_ITEM_COLLECTION, typename BATCH, typename EFFECT>
+        void emit(DRAW_ITEM_COLLECTION &result, BATCH *batch, EFFECT effect) {
+
+            if (dictionary.empty()) return;
+            
+            typename DRAW_ITEM_COLLECTION::value_type item(batch, effect);
+            for (Dictionary::iterator it = dictionary.begin(); it != dictionary.end(); ++it) {
+                if (it->second.empty()) continue;
+                
+                // expecting patchArrays is already sorted mostly.
+                std::sort(it->second.begin(), it->second.end(), PatchArrayComparator());
+                
+                for (OsdDrawContext::PatchArrayVector::iterator pit = it->second.begin(); pit != it->second.end(); ++pit) {
+                    if (not item.GetPatchArrays().empty()) {
+                        OsdDrawContext::PatchArray &back = item.GetPatchArrays().back();
+                        if (back.GetDescriptor() == pit->GetDescriptor() &&
+                            back.GetVertIndex() + back.GetNumIndices() == pit->GetVertIndex()) {
+                            // combine together
+                            back.SetNumPatches(back.GetNumPatches() + pit->GetNumPatches());
+                            continue;
+                        }
+                    }
+                    // append to item
+                    item.GetPatchArrays().push_back(*pit);
+                }
+            }
+
+            result.push_back(item);
+            dictionary.clear();
+        }
+
+        void allocate(OsdDrawContext::PatchArrayVector const &srcPatchArrays) {
+            for (OsdDrawContext::PatchArrayVector::const_iterator it = srcPatchArrays.begin(); it != srcPatchArrays.end(); ++it) {
+                dictionary.push_back(Dictionary::value_type(it->GetDescriptor(), OsdDrawContext::PatchArrayVector()));
+            }
+        }
+
+        void append(OsdDrawContext::PatchArray const &patchArray) {
+            for (Dictionary::iterator it = dictionary.begin(); it != dictionary.end(); ++it) {
+                if (patchArray.GetDescriptor() == it->first) {
+                    it->second.push_back(patchArray);
+                    return;
+                }
+            }
+        }
+        Dictionary dictionary;
+        
+    };
+
+    // ------------------------------------------------------------------------
+    // OptimizeDrawItem
+    // ------------------------------------------------------------------------
+    template <typename DRAW_ITEM_COLLECTION>
+    void OptimizeDrawItem(DRAW_ITEM_COLLECTION const &items,
+                          DRAW_ITEM_COLLECTION &result) {
+
+        typedef typename DRAW_ITEM_COLLECTION::value_type DrawItem;
+
+        typename DrawItem::BatchBase *currentBatch = NULL;
+        typename DrawItem::EffectHandle currentEffect; // XXX: initial value?
+
+        result.reserve(items.size());
+
+        PatchArrayCombiner combiner;
+        for (typename DRAW_ITEM_COLLECTION::const_iterator it = items.begin(); it != items.end(); ++it) {
+            typename DrawItem::BatchBase *batch = it->GetBatch();
+            typename DrawItem::EffectHandle effect = it->GetEffect();
+
+            if (currentBatch != batch || currentEffect != effect) {
+                // emit cached draw item
+                combiner.emit(result, currentBatch, currentEffect);
+                
+                currentBatch = batch;
+                currentEffect = effect;
+
+                // prepare new patch dictionary
+                combiner.allocate(batch->GetDrawContext()->patchArrays);
+            }
+
+            // merge consecutive items if possible. This operation changes drawing order.
+            // i.e.
+            //      PrimA-Regular, PrimA-Transition, PrimB-Regular, PrimB-Transition
+            // becomes
+            //      PrimA-Regular, PrimB-Regular, PrimA-Transition, PrimB-Transition
+            OsdDrawContext::PatchArrayVector const &patchArrays = it->GetPatchArrays();
+            for (OsdDrawContext::PatchArrayVector::const_iterator itp = patchArrays.begin(); itp != patchArrays.end(); ++itp) {
+                if (itp->GetNumPatches() == 0) continue;
+                // insert patchArrays into dictionary
+                // linear search is faster in this case so we don't use std::map.
+                combiner.append(*itp);
+            }
+        }
+        
+        // pick up after
+        combiner.emit(result, currentBatch, currentEffect);
+    }
+};
+
+
+}  // end namespace OPENSUBDIV_VERSION
+using namespace OPENSUBDIV_VERSION;
+
+}  // end namespace OpenSubdiv
+
+#endif  /* OSD_UTIL_DRAW_CONTROLLER_H */
