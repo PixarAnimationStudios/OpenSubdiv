e2bcc041d398a2635fb8448949f98c0aed1ae3a8
David G Yu <dyu@acm.org>
2014-10-24 11:07:24 -0700

    Added catmark_gregory_test{6,7} to a few examples.

--- examples/glEvalLimit/init_shapes.h
+++ examples/glEvalLimit/init_shapes.h
@@ -60,6 +60,9 @@ static std::vector<ShapeDesc> g_defaultShapes;
 #include <shapes/catmark_gregory_test2.h>
 #include <shapes/catmark_gregory_test3.h>
 #include <shapes/catmark_gregory_test4.h>
+#include <shapes/catmark_gregory_test5.h>
+#include <shapes/catmark_gregory_test6.h>
+#include <shapes/catmark_gregory_test7.h>
 #include <shapes/catmark_helmet.h>
 #include <shapes/catmark_hole_test1.h>
 #include <shapes/catmark_hole_test2.h>
@@ -104,6 +107,9 @@ static void initShapes() {
     g_defaultShapes.push_back( ShapeDesc("catmark_gregory_test2",    catmark_gregory_test2,    kCatmark ) );
     g_defaultShapes.push_back( ShapeDesc("catmark_gregory_test3",    catmark_gregory_test3,    kCatmark ) );
     g_defaultShapes.push_back( ShapeDesc("catmark_gregory_test4",    catmark_gregory_test4,    kCatmark ) );
+    g_defaultShapes.push_back( ShapeDesc("catmark_gregory_test5",    catmark_gregory_test5,    kCatmark ) );
+    g_defaultShapes.push_back( ShapeDesc("catmark_gregory_test6",    catmark_gregory_test6,    kCatmark ) );
+    g_defaultShapes.push_back( ShapeDesc("catmark_gregory_test7",    catmark_gregory_test7,    kCatmark ) );
     g_defaultShapes.push_back( ShapeDesc("catmark_hole_test1",       catmark_hole_test1,       kCatmark ) );
     g_defaultShapes.push_back( ShapeDesc("catmark_hole_test2",       catmark_hole_test2,       kCatmark ) );
     g_defaultShapes.push_back( ShapeDesc("catmark_pyramid_creases0", catmark_pyramid_creases0, kCatmark ) );
