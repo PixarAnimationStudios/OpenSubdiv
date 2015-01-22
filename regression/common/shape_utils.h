f0743d593be2424b96bc88323281f9e9c8859674
manuelk <mkraemer@pixar.com>
2015-01-22 11:08:13 -0800

    Add parsing ability for OBJ material format to regression/shape_utils
    - add simple parsing function to the Shape class
    - only a small portion of the MTL format is supported (no textures)
    - the feature is disabled by default
    - this work is incomplete : we still need to add piping to the GPU

--- regression/common/shape_utils.h
+++ regression/common/shape_utils.h
@@ -27,6 +27,7 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 //------------------------------------------------------------------------------
 enum Scheme {
@@ -38,10 +39,28 @@ enum Scheme {
 //------------------------------------------------------------------------------
 
 struct Shape {
+    // full(er) spec here: http://paulbourke.net/dataformats/mtl/
+    struct material {
+
+        material();
+
+        std::string name;
+
+        float ka[3],         // ambient
+              kd[3],         // diffuse
+              ks[3],         // specular
+              ns,            // specular exponent
+              ni,            // optical density (1.0=no refraction, glass=1.5)
+              sharpness,     // reflection sharpness
+              tf[3],         // transimission filter
+              d;             // dissolve factor (1.0 = opaque)
+
+        int illum;
+    };
 
     struct tag {
 
-        static tag * parseTag( char const * stream );
+        static tag * parseTag(char const * stream);
 
         std::string genTag() const;
 
@@ -51,7 +70,10 @@ struct Shape {
         std::vector<std::string> stringargs;
     };
 
-    static Shape * parseObj(char const * Shapestr, Scheme schme, int axis=1);
+    static Shape * parseObj(char const * Shapestr, Scheme schme,
+        int axis=1, bool parsemtl=false);
+
+    void parseMtllib(char const * stream);
 
     std::string genShape(char const * name) const;
 
@@ -64,20 +86,33 @@ struct Shape {
     int GetNumVertices() const { return (int)verts.size()/3; }
 
     int GetNumFaces() const { return (int)nvertsPerFace.size(); }
-    
+
     bool HasUV() const { return not (uvs.empty() or faceuvs.empty()); }
-    
+
     int GetFVarWidth() const { return HasUV() ? 2 : 0; }
 
-    std::vector<float>  verts;
-    std::vector<float>  uvs;
-    std::vector<float>  normals;
-    std::vector<int>    nvertsPerFace;
-    std::vector<int>    faceverts;
-    std::vector<int>    faceuvs;
-    std::vector<int>    facenormals;
-    std::vector<tag *>  tags;
-    Scheme              scheme;
+    std::vector<float>      verts;
+    std::vector<float>      uvs;
+    std::vector<float>      normals;
+    std::vector<int>        nvertsPerFace;
+    std::vector<int>        faceverts;
+    std::vector<int>        faceuvs;
+    std::vector<int>        facenormals;
+    std::vector<tag *>      tags;
+    Scheme                  scheme;
+
+    char FindMaterial(char const * name) {
+        for (int i=0; i<(int)mtls.size(); ++i) {
+            if (mtls[i]->name==name) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    std::string                 mtllib;
+    std::vector<unsigned short> mtlbind;
+    std::vector<material *>     mtls;
 };
 
 //------------------------------------------------------------------------------
