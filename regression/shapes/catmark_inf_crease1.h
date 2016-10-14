e21b4d902f9102128695d20766016cefe057b804
barry <barry.fowler@dreamworks.com>
2016-10-13 19:59:38 -0700

    Added a few more test shapes for face-varying and inf-sharp patches:     - added new shapes to regression/shapes     - updated shapes/all.h to include them     - updated init_shapes.h for glViewer and glFVarViewer

---
+++ regression/shapes/catmark_inf_crease1.h
@@ -0,0 +1,114 @@
+//
+//   Copyright 2016 Dreamworks
+//
+//   Licensed under the Apache License, Version 2.0 (the "Apache License")
+//   with the following modification; you may not use this file except in
+//   compliance with the Apache License and the following modification to it:
+//   Section 6. Trademarks. is deleted and replaced with:
+//
+//   6. Trademarks. This License does not grant permission to use the trade
+//      names, trademarks, service marks, or product names of the Licensor
+//      and its affiliates, except as required to comply with Section 4(c) of
+//      the License and to reproduce the content of the NOTICE file.
+//
+//   You may obtain a copy of the Apache License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the Apache License with the above modification is
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//   KIND, either express or implied. See the Apache License for the specific
+//   language governing permissions and limitations under the Apache License.
+//
+
+static const std::string catmark_inf_crease1 =
+"# This file uses centimeters as units for non-parametric coordinates.\n"
+"\n"
+"v -1.10  1.10  0.75\n"
+"v -0.10  1.10  0.00\n"
+"v -0.60  1.97  0.00\n"
+"v -1.60  1.97  0.00\n"
+"v -2.10  1.10  0.00\n"
+"v -1.60  0.23  0.00\n"
+"v -0.60  0.23  0.00\n"
+"v -0.35  1.60  0.00\n"
+"v -1.10  2.10  0.00\n"
+"v -1.85  1.60  0.00\n"
+"v -1.85  0.60  0.00\n"
+"v -1.10  0.10  0.00\n"
+"v -0.35  0.60  0.00\n"
+"v  1.10  1.10  0.75\n"
+"v  2.10  1.10  0.00\n"
+"v  1.60  1.97  0.00\n"
+"v  0.60  1.97  0.00\n"
+"v  0.10  1.10  0.00\n"
+"v  0.60  0.23  0.00\n"
+"v  1.60  0.23  0.00\n"
+"v  1.85  1.60  0.00\n"
+"v  1.10  2.10  0.00\n"
+"v  0.35  1.60  0.00\n"
+"v  0.35  0.60  0.00\n"
+"v  1.10  0.10  0.00\n"
+"v  1.85  0.60  0.00\n"
+"v -1.10 -1.10  0.75\n"
+"v -0.10 -1.10  0.00\n"
+"v -0.60 -0.23  0.00\n"
+"v -1.60 -0.23  0.00\n"
+"v -2.10 -1.10  0.00\n"
+"v -1.60 -1.97  0.00\n"
+"v -0.60 -1.97  0.00\n"
+"v -0.35 -0.60  0.00\n"
+"v -1.10 -0.10  0.00\n"
+"v -1.85 -0.60  0.00\n"
+"v -1.85 -1.60  0.00\n"
+"v -1.10 -2.10  0.00\n"
+"v -0.35 -1.60  0.00\n"
+"v  1.10 -1.10  0.75\n"
+"v  2.10 -1.10  0.00\n"
+"v  1.60 -0.23  0.00\n"
+"v  0.60 -0.23  0.00\n"
+"v  0.10 -1.10  0.00\n"
+"v  0.60 -1.97  0.00\n"
+"v  1.60 -1.97  0.00\n"
+"v  1.85 -0.60  0.00\n"
+"v  1.10 -0.10  0.00\n"
+"v  0.35 -0.60  0.00\n"
+"v  0.35 -1.60  0.00\n"
+"v  1.10 -2.10  0.00\n"
+"v  1.85 -1.60  0.00\n"
+"f  1  2  8  3\n"
+"f  1  3  9  4\n"
+"f  1  4 10  5\n"
+"f  1  5 11  6\n"
+"f  1  6 12  7\n"
+"f  1  7 13  2\n"
+"f 14 15 21 16\n"
+"f 14 16 22 17\n"
+"f 14 17 23 18\n"
+"f 14 18 24 19\n"
+"f 14 19 25 20\n"
+"f 14 20 26 15\n"
+"f 27 28 34 29\n"
+"f 27 29 35 30\n"
+"f 27 30 36 31\n"
+"f 27 31 37 32\n"
+"f 27 32 38 33\n"
+"f 27 33 39 28\n"
+"f 40 41 47 42\n"
+"f 40 42 48 43\n"
+"f 40 43 49 44\n"
+"f 40 44 50 45\n"
+"f 40 45 51 46\n"
+"f 40 46 52 41\n"
+"t interpolateboundary 1/0/0 1\n"
+"t crease 2/1/0   0  4  10.0\n"
+"t crease 2/1/0  13 15  10.0\n"
+"t crease 2/1/0  13 19  10.0\n"
+"t crease 2/1/0  26 28  10.0\n"
+"t crease 2/1/0  26 29  10.0\n"
+"t crease 2/1/0  26 31  10.0\n"
+"t crease 2/1/0  39 41  10.0\n"
+"t crease 2/1/0  39 44  10.0\n"
+"\n"
+;
