1ba1b4a31a89e642f6b9c6e831c217bdbec5f6d4
Dave Clyde <dave.clyde@disneyanimation.com>
2016-10-21 16:14:33 -0700

    try const stuff

--- opensubdiv/far/patchBasis.cpp
+++ opensubdiv/far/patchBasis.cpp
@@ -661,13 +661,13 @@ void GetGregoryWeights(PatchParamG<FD> const & param,
     }
 }
 
-template void GetBSplineWeights<float>(struct OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<float> const&, float, float, float* const, float* const, float* const, float* const, float* const, float* const);
-template void GetGregoryWeights<float>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<float> const&, float, float, float*, float*, float*, float*, float*, float*);
-template void GetBilinearWeights<float>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<float> const&, float, float, float*, float*, float*, float*, float*, float*);
+template void GetBSplineWeights<float>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<float> const&, float, float, float* const, float* const, float* const, float* const, float* const, float* const);
+template void GetGregoryWeights<float>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<float> const&, float, float, float* const, float* const, float* const, float* const, float* const, float* const);
+template void GetBilinearWeights<float>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<float> const&, float, float, float* const, float* const, float* const, float* const, float* const, float* const);
 
-template void GetBSplineWeights<double>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<double> const&, double, double, double*, double*, double*, double*, double*, double*);
-template void GetGregoryWeights<double>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<double> const&, double, double, double*, double*, double*, double*, double*, double*);
-template void GetBilinearWeights<double>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<double> const&, double, double, double*, double*, double*, double*, double*, double*);
+template void GetBSplineWeights<double>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<double> const&, double, double, double* const, double* const, double* const, double* const, double* const, double* const);
+template void GetGregoryWeights<double>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<double> const&, double, double, double* const, double* const, double* const, double* const, double* const, double* const);
+template void GetBilinearWeights<double>(OpenSubdiv::OPENSUBDIV_VERSION::Far::PatchParamG<double> const&, double, double, double* const, double* const, double* const, double* const, double* const, double* const);
 
 
 
