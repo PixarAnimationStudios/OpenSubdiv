079b1a9760ee61f8a5f4a5a0e633da292f4a2e39
barry <barry.fowler@dreamworks.com>
2019-05-31 16:05:13 -0700

    Added tetrahedron shape use in docmentation images and logo:     - added Catmark and Loop versions of the toroidal tetrahedron shape     - update shapes/all.h with the additions

---
+++ regression/shapes/catmark_toroidal_tet.h
@@ -0,0 +1,112 @@
+//
+//   Copyright 2019 DreamWorks Animation LLC.
+//
+//   Licensed under the Apache License, Version 2.0 (the "Apache License")
+//   with the following modification; you may not use this file except in
+//   compliance with the Apache License and the following modification to it:
+//   Section 6. Trademarks. is deleted and replaced with:
+//
+//   6. Trademarks. This License does not grant permission to use the trade
+//      names, trademarks, service marks, or product names of the Licensor
+//      and its affiliates, except as required to comply with Section 4(c) of
+//      the License and to reproduce the content of the NOTICE file.
+//
+//   You may obtain a copy of the Apache License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the Apache License with the above modification is
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//   KIND, either express or implied. See the Apache License for the specific
+//   language governing permissions and limitations under the Apache License.
+//
+
+static const std::string catmark_toroidal_tet =
+"#\n"
+"#  Toroidal tetrahedron oriented within an axially aligned cube:\n"
+"#\n"
+"v -1.0  1.0 -1.0\n"
+"v -1.0 -1.0  1.0\n"
+"v  1.0 -1.0 -1.0\n"
+"v  1.0  1.0  1.0\n"
+"v  0.2 -0.6 -0.6\n"
+"v -0.6  0.2 -0.6\n"
+"v -0.6 -0.6  0.2\n"
+"v -0.6  0.6 -0.2\n"
+"v -0.6 -0.2  0.6\n"
+"v  0.2  0.6  0.6\n"
+"v -0.2 -0.6  0.6\n"
+"v  0.6 -0.6 -0.2\n"
+"v  0.6  0.2  0.6\n"
+"v  0.6 -0.2 -0.6\n"
+"v -0.2  0.6 -0.6\n"
+"v  0.6  0.6  0.2\n"
+"v -0.4  0.4 -0.4\n"
+"v -0.4 -0.4  0.4\n"
+"v  0.4 -0.4 -0.4\n"
+"v  0.4  0.4  0.4\n"
+"\n"
+"vt 0.250 0.410\n"
+"vt 0.025 0.020\n"
+"vt 0.475 0.020\n"
+"vt 0.750 0.410\n"
+"vt 0.525 0.020\n"
+"vt 0.975 0.020\n"
+"vt 0.250 0.910\n"
+"vt 0.025 0.520\n"
+"vt 0.475 0.520\n"
+"vt 0.750 0.910\n"
+"vt 0.525 0.520\n"
+"vt 0.975 0.520\n"
+"vt 0.250 0.254\n"
+"vt 0.160 0.100\n"
+"vt 0.340 0.100\n"
+"vt 0.750 0.254\n"
+"vt 0.660 0.100\n"
+"vt 0.840 0.100\n"
+"vt 0.250 0.754\n"
+"vt 0.160 0.600\n"
+"vt 0.340 0.600\n"
+"vt 0.750 0.754\n"
+"vt 0.660 0.600\n"
+"vt 0.840 0.600\n"
+"vt 0.250 0.175\n"
+"vt 0.228 0.137\n"
+"vt 0.273 0.137\n"
+"vt 0.750 0.175\n"
+"vt 0.728 0.137\n"
+"vt 0.772 0.137\n"
+"vt 0.250 0.675\n"
+"vt 0.228 0.637\n"
+"vt 0.273 0.637\n"
+"vt 0.750 0.675\n"
+"vt 0.728 0.637\n"
+"vt 0.772 0.637\n"
+"\n"
+"f  1/1   3/2   5/14  6/13\n"
+"f  3/2   2/3   7/15  5/14\n"
+"f  2/3   1/1   6/13  7/15\n"
+"f  2/4   4/5  10/17  9/16\n"
+"f  4/5   1/6   8/18 10/17\n"
+"f  1/6   2/4   9/16  8/18\n"
+"f  3/7   4/8  13/20 12/19\n"
+"f  4/8   2/9  11/21 13/20\n"
+"f  2/9   3/7  12/19 11/21\n"
+"f  4/10  3/11 14/23 16/22\n"
+"f  3/11  1/12 15/24 14/23\n"
+"f  1/12  4/10 16/22 15/24\n"
+"f  6/13  5/14 19/26 17/25\n"
+"f  5/14  7/15 18/27 19/26\n"
+"f  7/15  6/13 17/25 18/27\n"
+"f  9/16 10/17 20/29 18/28\n"
+"f 10/17  8/18 17/30 20/29\n"
+"f  8/18  9/16 18/28 17/30\n"
+"f 12/19 13/20 20/32 19/31\n"
+"f 13/20 11/21 18/33 20/32\n"
+"f 11/21 12/19 19/31 18/33\n"
+"f 14/23 15/24 17/36 19/35\n"
+"f 15/24 16/22 20/34 17/36\n"
+"f 16/22 14/23 19/35 20/34\n"
+"\n"
+;
